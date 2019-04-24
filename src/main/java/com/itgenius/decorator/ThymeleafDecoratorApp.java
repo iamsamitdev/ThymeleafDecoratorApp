667434fd582d4e3676a4e8fa2931c4956dbabfca
Samit Koyom <samitkoyom@gmail.com>
2019-04-24 09:19:20 +0700

    initial commit

---
+++ src/main/java/com/itgenius/decorator/ThymeleafDecoratorApp.java
@@ -0,0 +1,13 @@
+package com.itgenius.decorator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafDecoratorApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafDecoratorApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/itgenius/decorator/ThymeleafDecoratorAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.itgenius.decorator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThymeleafDecoratorAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
